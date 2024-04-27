6b79fea69c10d205239eda1dd5e5e15b39ba0fa8
Sanju Channabasappa <48892510+SanjuChannabasappa@users.noreply.github.com>
2024-04-27 13:45:07 +0000

    datatypes

---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,10 @@
+"use strict"; // treat all the JS code as newer version
+
+
+// number => 2
+// bigint
+// string => "sanju"
+// boolean => false , true
+// null => standalone value
+// undefined  =>
+// symbol => unique
