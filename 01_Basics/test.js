f9405c0c5ca28a69828393cabea6467337c30485
Sanju Channabasappa <48892510+SanjuChannabasappa@users.noreply.github.com>
2024-04-27 13:29:45 +0000

    Commit basics

--- 01_Basics/test.js
+++ 01_Basics/test.js
@@ -1,1 +1,17 @@
-console.log("sanju")
+const accountId = 12345;
+let accountName = "sanju";
+var accountEmail = "sanju@c.com";
+accountCity = "Bengalure";
+let accontStatus;
+
+console.table([accountId,accountName,accountEmail,accountCity,accontStatus]);
+
+
+
+
+{
+    var i = 1;
+    console.log(i);
+}
+
+console.log(i);
