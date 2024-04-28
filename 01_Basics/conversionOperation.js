3c84fc37f365f7a9ccac940fca790b11f48f81c3
Sanju Channabasappa <48892510+SanjuChannabasappa@users.noreply.github.com>
2024-04-28 14:47:30 +0000

    conversion of datatypes

---
+++ 01_Basics/conversionOperation.js
@@ -0,0 +1,70 @@
+/* converting to number  */
+
+// let score = undefined;
+// console.log(typeof score);
+// score = Number(score);            // converting string to number
+// console.log(typeof score);
+// console.log(score)
+
+
+// "33" - 33
+// "33abc" - Nan
+// "" - 0
+// null - 0
+// undefined - NaN
+// true - 1
+// false - 0
+
+
+/* Boolean conversion */
+
+// let loggedIn = undefined;
+// loggedInBoolean = Boolean(loggedIn);
+// console.log(typeof loggedInBoolean);
+// console.log(loggedInBoolean);
+
+// 1 - true , 22 - true , 0 - false
+// "sanju" - true , "" - false
+// null - false , undefined - false
+
+/* String conversion */
+
+// let str;
+// let convstr = String(str);
+// console.log(typeof convstr);
+// console.log(str);
+
+// 1 - 1 , 0 - 0 , "" - ""
+
+
+/* operations */
+
+// let value = 3;
+// let negValue = -3;
+// console.log(negValue);
+// console.log(typeof negValue);
+
+/* string concatenations */
+// let str1 = "hello";
+// let str2 = " sanju";
+// let str3 = str1 + str2;
+// console.log(str3);
+
+
+// console.log(1+2);
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"3");
+
+
+
+/* prefix operation */
+// let gamecounter = 0;
+// console.log(++gamecounter);
+// console.log(gamecounter);
+
+/* postfix operation */
+// let gamecounter = 0;
+// console.log(gamecounter++);
+// console.log(gamecounter);
