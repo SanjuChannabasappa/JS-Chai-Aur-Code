0ab1fe121b76c58d28c8195ace95b296ba1e97a3
Sanju Channabasappa <48892510+SanjuChannabasappa@users.noreply.github.com>
2024-04-28 16:02:51 +0000

    datatypes summary

--- 01_Basics/datatypes summary.js
+++ 01_Basics/datatypes summary.js
@@ -10,3 +10,26 @@
 const id = Symbol("123");
 const anotherId = Symbol("123");
 console.log(id === anotherId);
+
+
+/* BigInt */
+const bignumber = 123456789n;
+console.log(typeof bignumber); 
+
+/* Array */
+const heros = ["sanju", "siddu", "sanjusiddu"];
+
+
+/* Object */
+const data = {
+    name : "sanju",
+    age : 23,
+    work : "soft_eng"
+};
+
+
+/* functions */
+const simple = function () {
+    console.log("sanju")
+    
+}
