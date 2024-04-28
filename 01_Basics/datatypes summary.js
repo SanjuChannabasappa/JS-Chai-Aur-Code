76bc6d3b53a8873fa39256359ccb1e0e2d2cecb6
Sanju Channabasappa <48892510+SanjuChannabasappa@users.noreply.github.com>
2024-04-28 15:38:46 +0000

    datatypes summary

---
+++ 01_Basics/datatypes summary.js
@@ -0,0 +1,12 @@
+/* Primitive */
+// 7 types : String , Number, Boolean, null, undefined, Symbol, BigInt
+
+
+/* Non-primitive (Reference) */
+// Array, Object, functions
+
+
+/* Symbol */
+const id = Symbol("123");
+const anotherId = Symbol("123");
+console.log(id === anotherId);
